7fe14febea93138ea9a7e440cff308f489bff4ff
devduart <walysson.duarte@gmail.com>
2023-12-02 00:58:15 -0300

    first day adventcode

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,32 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
+fun part1(input: List<String>) = input.sumOf { it.extractValue() }
+
+fun part2(input: List<String>) = input.sumOf { it.calculateValue() }
+
+fun String.extractValue(): Int =
+    (find {it.isDigit()}?.digitToInt() ?:0) * 10 + (findLast { it.isDigit() }?.digitToInt() ?: 0)
+
+private val textToDigit = mapOf(
+    "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5,
+    "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9
+)
+private fun String.calculateValue(): Int =
+    findFirstOrLastValue(true) * 10 + findFirstOrLastValue(false)
+
+private fun String.findFirstOrLastValue(isFirst: Boolean): Int {
+    val indices = if (isFirst) indices else indices.reversed()
+    return indices.asSequence()
+        .mapNotNull { index -> getValueAt(index) }
+        .firstOrNull() ?: 0
+}
+
+private fun String.getValueAt(index: Int): Int? =
+    when {
+        this[index].isDigit() -> Character.getNumericValue(this[index])
+        else -> textToDigit.entries.firstOrNull { startsWith(it.key, index) }?.value
+    }

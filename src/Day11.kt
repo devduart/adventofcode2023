4da96dcad107d50bff1711815b2a681de0ba31c0
devduart <walysson.duarte@gmail.com>
2023-12-15 17:52:44 -0300

    refactor: day11

--- src/Day11.kt
+++ src/Day11.kt
@@ -7,62 +7,47 @@ fun main() {
 data class Position(var x: Long, var y: Long)
 
 private fun part1(input: List<String>): Long {
-    val galaxies = mutableListOf<Position>()
-    val blankRows = mutableSetOf<Int>()
-    val blankCols = mutableSetOf<Int>()
+    val galaxies = input.mapIndexed { row, line ->
+        line.mapIndexedNotNull { col, ch -> if (ch == '#') Position(row.toLong(), col.toLong()) else null }
+    }.flatten()
 
-    input.forEachIndexed { row, line ->
-        line.forEachIndexed { col, ch ->
-            if (ch == '#') galaxies.add(Position(row.toLong(), col.toLong()))
-        }
+    val blankRows = input.indices.filter { row -> input[row].all { it == '.' } }.toSet()
+    val blankCols = input[0].indices.filter { col -> input.all { it[col] == '.' } }.toSet()
+
+    fun adjustedPosition(position: Position): Position {
+        val adjustedX = position.x + blankRows.count { it < position.x }
+        val adjustedY = position.y + blankCols.count { it < position.y }
+        return Position(adjustedX, adjustedY)
     }
 
-    input.indices.filter { row -> input[row].all { it == '.' } }.forEach { blankRows.add(it) }
-    input[0].indices.filter { col -> input.all { it[col] == '.' } }.forEach { blankCols.add(it) }
-
-    galaxies.forEach { galaxy ->
-        galaxy.x += blankRows.count { it < galaxy.x }
-        galaxy.y += blankCols.count { it < galaxy.y }
+    fun distance(p1: Position, p2: Position): Long {
+        val adjustedP1 = adjustedPosition(p1)
+        val adjustedP2 = adjustedPosition(p2)
+        return kotlin.math.abs(adjustedP1.x - adjustedP2.x) + kotlin.math.abs(adjustedP1.y - adjustedP2.y)
     }
 
-    var totalDist: Long = 0
-    for (i in galaxies.indices) {
-        for (j in i until galaxies.size) {
-            totalDist += kotlin.math.abs(galaxies[i].x - galaxies[j].x) +
-                    kotlin.math.abs(galaxies[i].y - galaxies[j].y)
-        }
+    return galaxies.asSequence().withIndex().sumOf { (i, galaxy1) ->
+        galaxies.drop(i + 1).sumOf { galaxy2 -> distance(galaxy1, galaxy2) }
     }
-
-    return totalDist
 }
 
 private fun part2(input: List<String>): Long {
-    val galaxies = mutableListOf<Position>()
-    val blankRows = mutableSetOf<Int>()
-    val blankCols = mutableSetOf<Int>()
+    val galaxies = input.mapIndexed { row, line ->
+        line.mapIndexedNotNull { col, ch -> if (ch == '#') Position(row.toLong(), col.toLong()) else null }
+    }.flatten()
 
-    input.forEachIndexed { row, line ->
-        line.forEachIndexed { col, ch ->
-            if (ch == '#') galaxies.add(Position(row.toLong(), col.toLong()))
-        }
+    val blankRows = input.indices.filter { row -> input[row].all { it == '.' } }.toSet()
+    val blankCols = input[0].indices.filter { col -> input.all { it[col] == '.' } }.toSet()
+
+    fun adjustedDistance(p1: Position, p2: Position): Long {
+        val dx = kotlin.math.abs(p1.x - p2.x) + blankRows.count { it in p1.x until p2.x || it in p2.x until p1.x } * 999999
+        val dy = kotlin.math.abs(p1.y - p2.y) + blankCols.count { it in p1.y until p2.y || it in p2.y until p1.y } * 999999
+        return dx + dy
     }
 
-    input.indices.filter { row -> input[row].all { it == '.' } }.forEach { blankRows.add(it) }
-    input[0].indices.filter { col -> input.all { it[col] == '.' } }.forEach { blankCols.add(it) }
-
-    galaxies.forEach { galaxy ->
-        galaxy.x += blankRows.count { it < galaxy.x } * 999999
-        galaxy.y += blankCols.count { it < galaxy.y } * 999999
+    return galaxies.asSequence().withIndex().sumOf { (i, galaxy1) ->
+        galaxies.drop(i + 1).sumOf { galaxy2 -> adjustedDistance(galaxy1, galaxy2) }
     }
-
-    var totalDist: Long = 0
-    for (i in galaxies.indices) {
-        for (j in i until galaxies.size) {
-            totalDist += kotlin.math.abs(galaxies[i].x - galaxies[j].x) +
-                    kotlin.math.abs(galaxies[i].y - galaxies[j].y)
-        }
-    }
-
-    return totalDist
 }
+
 

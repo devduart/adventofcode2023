8631a6119e361f2c83fc23f6b130b2b656ad960b
devduart <walysson.duarte@gmail.com>
2023-12-15 18:54:42 -0300

    feat: day12 part2

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,8 +1,8 @@
 fun main() {
     val input = readInput("Day12")
     println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
-
 
 private fun part1(input: List<String>): Int {
     return countArrangements(input)
@@ -41,3 +41,56 @@ fun isValid(springs: String, groupSizes: List<Int>): Boolean {
     val actualSizes = springs.trim('.').split(".").filter { it.isNotEmpty() }.map { it.length }
     return actualSizes == groupSizes
 }
+
+
+private fun part2(input: List<String>): Long {
+    return input
+        .map { parseRow(it) }
+        .map { unfold(it) }
+        .sumOf { (springs, damage) -> countArrangements2(springs, damage) }
+}
+
+private fun countArrangements2(
+    springs: String,
+    damage: List<Int>,
+    cache: MutableMap<Pair<String, List<Int>>, Long> = HashMap()
+): Long {
+    val key = springs to damage
+    cache[key]?.let {
+        return it
+    }
+    if (springs.isEmpty()) return if (damage.isEmpty()) 1 else 0
+
+    return when (springs.first()) {
+        '.' -> countArrangements2(springs.dropWhile { it == '.' }, damage, cache)
+
+        '?' -> countArrangements2(springs.substring(1), damage, cache) +
+                countArrangements2("#${springs.substring(1)}", damage, cache)
+
+        '#' -> when {
+            damage.isEmpty() -> 0
+            else -> {
+                val thisDamage = damage.first()
+                val remainingDamage = damage.drop(1)
+                if (thisDamage <= springs.length && springs.take(thisDamage).none { it == '.' }) {
+                    when {
+                        thisDamage == springs.length -> if (remainingDamage.isEmpty()) 1 else 0
+                        springs[thisDamage] == '#' -> 0
+                        else -> countArrangements2(springs.drop(thisDamage + 1), remainingDamage, cache)
+                    }
+                } else 0
+            }
+        }
+
+        else -> throw IllegalStateException("Invalid springs: $springs")
+    }.apply {
+        cache[key] = this
+    }
+}
+private fun unfold(input: Pair<String, List<Int>>): Pair<String, List<Int>> =
+    (0..4).joinToString("?") { input.first } to (0..4).flatMap { input.second }
+
+private fun parseRow(input: String): Pair<String, List<Int>> =
+    input.split(" ").run {
+        first() to last().split(",").map { it.toInt() }
+    }
